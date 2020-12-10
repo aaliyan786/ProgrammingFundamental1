62615aa4dfae5fd42a5b2d8134f6f84a0cf88c2d
aaliyan786 <maaliyankhan@hotmail.com>
2020-12-11 01:34:44 +0500

    updated

--- vid3 user_inp str read.c
+++ vid3 user_inp str read.c
@@ -4,13 +4,14 @@ void main()
 {
 	int i;
 	FILE *fp;
-	char ch[100];
+	char ch[10];
+	printf("%s",ch);
 	fp = fopen("file.txt","r");
 	if(fp == NULL)
 	{
 		printf("cannot open file");
 	}
-	while(fgets(ch,100,fp)!=NULL)
+	while(fgets(ch,13,fp)!=NULL)
 	{
 		printf("%s",ch);
 	}
